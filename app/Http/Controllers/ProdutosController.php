25a300c1964db265f3cba7c4061a0b601dcfa11c
LeoReAlert <leonardocriacaoweb2021@gmail.com>
2022-08-20 07:38:54 -0400

    first commit

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutosController extends Controller
+{
+    public function create ()
+    {
+       return view('produtos.create');
+
+    }
+}
---
+++ resources/views/produtos/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Cadastrar um novo produto</title>
+</head>
+<body>
+<form action="">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome"> <br  />
+    <label for="custo">Custo</label>
+    <input type="text" name="custo"><br  />
+    <label for="preco">Preço</label>
+    <input type="text" name="preco"><br  />
+    <label for="">Quantidade</label>
+    <input type="text" name="quantidade"><br   />
+    <button>Salvar</button>
+
+
+</body>
+</html>
